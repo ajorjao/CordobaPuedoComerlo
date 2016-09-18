aa3685d81d745577f2a4a2dd51538c86bf8632ce
Jorge Caullan <jorge.caullan.12@sansano.usm.cl>
2016-09-18 03:45:14 -0300

    vista para registrarse, mejorada

--- www/js/vistas/crear_perfil.js
+++ www/js/vistas/crear_perfil.js
@@ -1,87 +1,95 @@
 function register(){
-	var form = new FormData();
-	form.append("user[email]", $("#email").val());
-	form.append("user[password]", $("#pass").val());
-	form.append("user[password_confirmation]", $("#pass").val());
+  var form = new FormData();
+  form.append("user[email]", $("#email").val());
+  form.append("user[password]", $("#pass").val());
+  form.append("user[password_confirmation]", $("#pass_conf").val());
 
-	var settings = {
-	  "async": true,
-	  "crossDomain": true,
-	  "url": "http://"+url_server+"/register",
-	  "method": "POST",
-	  xhrFields: {
-	    withCredentials: true
-	  },
-	  "headers": {
-	    "cache-control": "no-cache",
-	    "postman-token": "6929ffa6-6171-2dfa-13b0-19fcaafe723b"
-	  },
-	  "processData": false,
-	  "contentType": false,
-	  "mimeType": "multipart/form-data",
-	  "data": form,
-	  error: function(resp, status){
-	  	// console.log(resp);
-    	// console.log("error al logearse");
-    	// alert("Error, por favor comprueba tu conexión")
-      if (resp.status==0){
-        alert("Error, por favor comprueba tu conexión")
-      }
-      else{
-        alert(JSON.parse(resp.responseText).error)
-      }
-	  }
+  var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "http://"+url_server+"/register",
+		"method": "POST",
+		xhrFields: {
+		  withCredentials: true
+		},
+		"headers": {
+		  "cache-control": "no-cache",
+		  "postman-token": "6929ffa6-6171-2dfa-13b0-19fcaafe723b"
+		},
+		"processData": false,
+		"contentType": false,
+		"mimeType": "multipart/form-data",
+		"data": form,
+		error: function(resp, status){
+		  // console.log(resp);
+		  // console.log("error al logearse");
+		  // alert("Error, por favor comprueba tu conexión")
+		  if (resp.status==0){
+				alert("Error, por favor comprueba tu conexión");
+		  }
+		  else{
+				alert(JSON.parse(resp.responseText).error);
+		  }
+		}
 	}
 
-	$.ajax(settings).done(function (response) {
-	  console.log(response);
-	  add_new_familiar();
-	});
+	if ($('#pass').val() != $('#pass_conf').val()) {
+		alert("Error, las contraseñas no coinciden")
+	}
+	else if ($('#name').val()=="") {
+		alert("Error, tu nombre no puede quedar vacio")
+	}
+	else {
+	  $.ajax(settings).done(function (response) {
+			console.log(response);
+			add_new_familiar();
+	  });
+	}
 }
 
 function add_new_familiar(){
-	var form = new FormData();
-	form.append("family[name]", $("#name").val());
+  var form = new FormData();
+  form.append("family[name]", $("#name").val());
 
-	var settings = {
-	  "async": true,
-	  "crossDomain": true,
-	  "url": "http://"+url_server+"/families",
-	  "method": "POST",
-	  xhrFields: {
-	    withCredentials: true
-	  },
-	  "headers": {
-	    "cache-control": "no-cache",
-	    "postman-token": "880451d2-bb70-c49a-a2a2-bdea0c3d7827"
-	  },
-	  "processData": false,
-	  "contentType": false,
-	  "mimeType": "multipart/form-data",
-	  "data": form,
-	  error: function(resp, status){
-	  	// console.log(resp);
-      alert("Error, por favor comprueba tu conexión");
-      location.reload();
-	  }
+  var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "http://"+url_server+"/families",
+	"method": "POST",
+	xhrFields: {
+	  withCredentials: true
+	},
+	"headers": {
+	  "cache-control": "no-cache",
+	  "postman-token": "880451d2-bb70-c49a-a2a2-bdea0c3d7827"
+	},
+	"processData": false,
+	"contentType": false,
+	"mimeType": "multipart/form-data",
+	"data": form,
+	error: function(resp, status){
+	  // console.log(resp);
+	  alert("Error, por favor comprueba tu conexión");
+	  location.reload();
 	}
+  }
 
-	$.ajax(settings).done(function (response) {
-	  // console.log(response);
+  $.ajax(settings).done(function (response) {
+		// console.log(response);
 
-    var intolerances = [];
-    $('.btn-add-intolerances').find('input:checked').each(function() {
-      intolerances.push($(this).data('value'));
-    });
-    new_intolerances(JSON.parse(response).created.id, intolerances);
-
-	});
+		var intolerances = [];
+		$("#check-list-box li.active").each(function() {
+		  intolerances.push($(this).data('value'));
+		});
+		new_intolerances(JSON.parse(response).created.id, intolerances);
+		alert("Te has registrado exitosamente");
+  });
 }
 
 function new_intolerances(familiar, intolerancias){ //(int, array)
   if (intolerancias==0 && familiar==0){
-    familiar = stored_id;
-    intolerancias = $(".select-intolerance").val();
+		familiar = stored_id;
+		intolerancias = $(".select-intolerance").val();
   }
 
   var form = new FormData();
@@ -89,41 +97,131 @@ function new_intolerances(familiar, intolerancias){ //(int, array)
   form.append("intolerances_ids", intolerancias.toString());
 
   var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "http://"+url_server+"/family/intolerance",
-    "method": "POST",
-    xhrFields: {
-      withCredentials: true
-    },
-    "headers": {
-      "cache-control": "no-cache",
-      "postman-token": "04643411-9b85-17f5-0c7f-4c4375d05851"
-    },
-    "processData": false,
-    "contentType": false,
-    "mimeType": "multipart/form-data",
-    "data": form,
-    error: function(resp, status){
-      // alert("Error, no se pudieron agregar las intolerancias, por favor comprueba tu conexión")
-      // console.log(resp);
-      if (resp.status==0){
-        alert("Error, por favor comprueba tu conexión")
-      }
-      else{
-        alert(JSON.parse(resp.responseText).error)
-      }
-      location.reload();
-    }
+	"async": false,
+	"crossDomain": true,
+	"url": "http://"+url_server+"/family/intolerance",
+	"method": "POST",
+	xhrFields: {
+	  withCredentials: true
+	},
+	"headers": {
+	  "cache-control": "no-cache",
+	  "postman-token": "04643411-9b85-17f5-0c7f-4c4375d05851"
+	},
+	"processData": false,
+	"contentType": false,
+	"mimeType": "multipart/form-data",
+	"data": form,
+	error: function(resp, status){
+	  // alert("Error, no se pudieron agregar las intolerancias, por favor comprueba tu conexión")
+	  // console.log(resp);
+	  if (resp.status==0){
+		alert("Error, por favor comprueba tu conexión")
+	  }
+	  else{
+		alert(JSON.parse(resp.responseText).error)
+	  }
+	  location.reload();
+	}
   }
 
   $.ajax(settings).done(function (response) {
-    console.log(response);
-    window.location = "perfil.html";
-    // location.reload();
+	console.log(response);
+	window.location = "perfil.html";
+	// location.reload();
   });
 }
 
 function go_login(){
   window.location = "login.html";
 }
+
+
+
+
+function showhidepass(){
+	if ($('#pass').attr('type') == 'password'){
+		$('#pass').attr('type', 'text');
+		$('#pass_conf').attr('type', 'text');
+	}
+	else{
+		$('#pass').attr('type', 'password');
+		$('#pass_conf').attr('type', 'password');
+	}
+  
+}
+
+
+
+
+
+// funcion que hace que los checklist se vean bien... no es necesario ententerla, solo saber q funciona
+$(function () {
+	$('.list-group.checked-list-box .list-group-item').each(function () {
+		
+		// Settings
+		var $widget = $(this),
+			$checkbox = $('<input type="checkbox" class="hidden" />'),
+			color = ($widget.data('color') ? $widget.data('color') : "primary"),
+			style = ($widget.data('style') == "button" ? "btn-" : "list-group-item-"),
+			settings = {
+				on: {
+					icon: 'glyphicon glyphicon-check'
+				},
+				off: {
+					icon: 'glyphicon glyphicon-unchecked'
+				}
+			};
+			
+		$widget.css('cursor', 'pointer')
+		$widget.append($checkbox);
+
+		// Event Handlers
+		$widget.on('click', function () {
+			$checkbox.prop('checked', !$checkbox.is(':checked'));
+			$checkbox.triggerHandler('change');
+			updateDisplay();
+		});
+		$checkbox.on('change', function () {
+			updateDisplay();
+		});
+		  
+
+		// Actions
+		function updateDisplay() {
+			var isChecked = $checkbox.is(':checked');
+
+			// Set the button's state
+			$widget.data('state', (isChecked) ? "on" : "off");
+
+			// Set the button's icon
+			$widget.find('.state-icon')
+				.removeClass()
+				.addClass('state-icon ' + settings[$widget.data('state')].icon);
+
+			// Update the button's color
+			if (isChecked) {
+				$widget.addClass(style + color + ' active');
+			} else {
+				$widget.removeClass(style + color + ' active');
+			}
+		}
+
+		// Initialization
+		function init() {
+			
+			if ($widget.data('checked') == true) {
+				$checkbox.prop('checked', !$checkbox.is(':checked'));
+			}
+			
+			updateDisplay();
+
+			// Inject the icon if applicable
+			if ($widget.find('.state-icon').length == 0) {
+				$widget.prepend('<span class="state-icon ' + settings[$widget.data('state')].icon + '"></span>');
+			}
+		}
+		init();
+	});
+
+});
--- www/js/vistas/perfil.js
+++ www/js/vistas/perfil.js
@@ -1,31 +1,30 @@
 function logout(){
 	var settings = {
-	  "async": true,
-	  "crossDomain": true,
-	  "url": "http://"+url_server+"/sign_out",
-	  "method": "DELETE",
-	  xhrFields: {
-	    withCredentials: true
-	  },
-	  "headers": {
-	    "cache-control": "no-cache",
-	    "postman-token": "0d3ea712-a98e-209b-6bcc-8b064906ed01"
-	  },
-	  error: function(resp, status){
-	  	// console.log(resp);
-    	console.log("error al cerrar sesion");
-    	alert("Error al cerrar sesion, por favor comprueba tu conexión")
-	  }
+		"async": true,
+		"crossDomain": true,
+		"url": "http://"+url_server+"/sign_out",
+		"method": "DELETE",
+		xhrFields: {
+			withCredentials: true
+		},
+		"headers": {
+			"cache-control": "no-cache",
+			"postman-token": "0d3ea712-a98e-209b-6bcc-8b064906ed01"
+		},
+		error: function(resp, status){
+			// console.log(resp);
+			console.log("error al cerrar sesion");
+			alert("Error al cerrar sesion, por favor comprueba tu conexión")
+		}
 	}
 
-	
-  var logout = confirm("¿Estás seguro que deseas cerrar sesion?");
-  if (logout == true) {
+
+	var logout = confirm("¿Estás seguro que deseas cerrar sesion?");
+	if (logout == true) {
 		$.ajax(settings).done(function (response) {
-      alert("Cerraste sesion satisfactoriamente");
 			window.location = "login.html";
 		});
-  }
+	}
 }
 
 var stored_id = 0;
@@ -39,35 +38,35 @@ function guardar_id(newid, name){
 function add_familiar(nombre, id, importante){
 	var familiar = ''+
 		'<div class="panel panel-default">'+
-	    '<div class="panel-heading" role="tab" id="heading_'+id+'">'+
-	      '<h4 class="panel-title">'+
-	        '<a role="button" data-toggle="collapse" data-parent="#accordion" onClick="guardar_id('+id+',\''+nombre+'\')" href="#collapse_'+id+'" aria-expanded="false" aria-controls="collapseOne">'+
-	          nombre+
-	          '<a data-toggle="modal" onClick="guardar_id('+id+',\''+nombre+'\')" data-target="#nombre-familiar-modal" href=""><span class=" edit-relative glyphicon glyphicon-pencil aria-hidden="true"></span></a>'+
-	        '</a>'+
-	      '</h4>'+
-	    '</div>'+
+			'<div class="panel-heading" role="tab" id="heading_'+id+'">'+
+				'<h4 class="panel-title">'+
+					'<a role="button" data-toggle="collapse" data-parent="#accordion" onClick="guardar_id('+id+',\''+nombre+'\')" href="#collapse_'+id+'" aria-expanded="false" aria-controls="collapseOne">'+
+						nombre+
+						'<a data-toggle="modal" onClick="guardar_id('+id+',\''+nombre+'\')" data-target="#nombre-familiar-modal" href=""><span class=" edit-relative glyphicon glyphicon-pencil aria-hidden="true"></span></a>'+
+					'</a>'+
+				'</h4>'+
+			'</div>'+
 
 
-	    '<div id="collapse_'+id+'" class="panel-collapse collapse'+importante+'" role="tabpanel" aria-labelledby="heading_'+id+'">'+
-	      '<div class="panel-body">'+
-	        '<ul class="list-group-'+id+'">'+
-			    // #############intolerancias###############
-					  // '<li class="intolerance list-group-item">'+
-					  // 	'<img src="img/intolerancias/2.png" alt="...">'+
-					  //   'Nueces'+
-					  //   '<a href=""><span class=" remove-intolerance glyphicon glyphicon-remove aria-hidden="true"></span></a>'+
-					  // '</li>'+
-			    // #############intolerancias###############
+			'<div id="collapse_'+id+'" class="panel-collapse collapse'+importante+'" role="tabpanel" aria-labelledby="heading_'+id+'">'+
+				'<div class="panel-body">'+
+					'<ul class="list-group-'+id+'">'+
+					// #############intolerancias###############
+						// '<li class="intolerance list-group-item">'+
+						// 	'<img src="img/intolerancias/2.png" alt="...">'+
+						//   'Nueces'+
+						//   '<a href=""><span class=" remove-intolerance glyphicon glyphicon-remove aria-hidden="true"></span></a>'+
+						// '</li>'+
+					// #############intolerancias###############
 
-					  // '<button data-toggle="modal" data-target="#intolerancia-modal" type="button" class="list-group-item">'+
+						// '<button data-toggle="modal" data-target="#intolerancia-modal" type="button" class="list-group-item">'+
 						 //  '<a  href=""><span class=" add-intolerance glyphicon glyphicon-plus aria-hidden="true"></span></a>'+
 						 //  'Agregar Intolerancia'+
-					  // '</button>'+
+						// '</button>'+
 					'</ul>'+
-	      '</div>'+
-	    '</div>'+
-	  '</div>'
+				'</div>'+
+			'</div>'+
+		'</div>'
 	$(".panel-group").append(familiar);
 }
 
@@ -77,12 +76,12 @@ function add_intolerance(intolerance_id, family_id){
 	intolerance_name = all_intolerances[intolerance_id-1]
 
 	var intolerance = ''+
-  '<li class="intolerance list-group-item">'+
-  	'<img src="img/intolerancias/'+intolerance_id+'.png" alt="..."> '+intolerance_name+
-    // '<a href="#"><span class="remove-intolerance glyphicon glyphicon-remove aria-hidden="true"  onClick="delete_intolerance('+intolerance_id+')"></span></a>'+
-    '<a href="#"><span class="remove-intolerance glyphicon glyphicon-remove aria-hidden="true"  onClick="delete_intolerance('+intolerance_id+')"></span></a>'+
-  '</li>'
-  $(".list-group-"+family_id).append(intolerance)
+	'<li class="intolerance list-group-item">'+
+		'<img src="img/intolerancias/'+intolerance_id+'.png" alt="..."> '+intolerance_name+
+		// '<a href="#"><span class="remove-intolerance glyphicon glyphicon-remove aria-hidden="true"  onClick="delete_intolerance('+intolerance_id+')"></span></a>'+
+		'<a href="#"><span class="remove-intolerance glyphicon glyphicon-remove aria-hidden="true"  onClick="delete_intolerance('+intolerance_id+')"></span></a>'+
+	'</li>'
+	$(".list-group-"+family_id).append(intolerance)
 }
 
 // como family_id se usa el stored_id
@@ -92,22 +91,22 @@ function delete_intolerance(intolerance_id){
 	form.append("intolerance_id", intolerance_id);
 
 	var settings = {
-	  "async": true,
-	  "crossDomain": true,
-	  "url": "http://"+url_server+"/family/intolerance",
-	  "method": "DELETE",
-	  xhrFields: {
-	    withCredentials: true
-	  },
-	  "headers": {
-	    "cache-control": "no-cache",
-	    "postman-token": "8b5643a5-12a1-00bb-f3fb-155a6197ace5"
-	  },
-	  "processData": false,
-	  "contentType": false,
-	  "mimeType": "multipart/form-data",
-	  "data": form,
-	  error: function(resp, status){
+		"async": true,
+		"crossDomain": true,
+		"url": "http://"+url_server+"/family/intolerance",
+		"method": "DELETE",
+		xhrFields: {
+			withCredentials: true
+		},
+		"headers": {
+			"cache-control": "no-cache",
+			"postman-token": "8b5643a5-12a1-00bb-f3fb-155a6197ace5"
+		},
+		"processData": false,
+		"contentType": false,
+		"mimeType": "multipart/form-data",
+		"data": form,
+		error: function(resp, status){
 			if (resp.status==0){
 				alert("Error, por favor comprueba tu conexión")
 			}
@@ -115,36 +114,36 @@ function delete_intolerance(intolerance_id){
 				alert(JSON.parse(resp.responseText).error)
 			}
 			location.reload();
-	  }
+		}
 	}
 
-  var eliminar_intolerancia = confirm("¿Estás seguro que deseas eliminar la intolerancia de "+stored_name+"?");
-  if (eliminar_intolerancia == true) {
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-      alert("Intolerancia eliminada");
+	var eliminar_intolerancia = confirm("¿Estás seguro que deseas eliminar la intolerancia de "+stored_name+"?");
+	if (eliminar_intolerancia == true) {
+		$.ajax(settings).done(function (response) {
+			console.log(response);
+			alert("Intolerancia eliminada");
 			location.reload();
-    });
-  }
+		});
+	}
 }
 
 function get_my_data(){
 	var settings = {
-	  "async": true,
-	  "crossDomain": true,
-	  "url": "http://"+url_server+"/user",
-	  "method": "GET",
-	  xhrFields: {
-	    withCredentials: true
-	  },
-	  "headers": {
-	    "cache-control": "no-cache",
-	    "postman-token": "e75d6d1f-85a5-fdce-0ff6-704ff358920b"
-	  },
-	  error: function(resp, status){
-      alert("Error de conexión, intentalo nuevamente");
-      window.location = "login.html";
-	  }
+		"async": true,
+		"crossDomain": true,
+		"url": "http://"+url_server+"/user",
+		"method": "GET",
+		xhrFields: {
+			withCredentials: true
+		},
+		"headers": {
+			"cache-control": "no-cache",
+			"postman-token": "e75d6d1f-85a5-fdce-0ff6-704ff358920b"
+		},
+		error: function(resp, status){
+			alert("Error de conexión, intentalo nuevamente");
+			window.location = "login.html";
+		}
 	}
 
 	$.ajax(settings).done(function (response) {
@@ -152,7 +151,7 @@ function get_my_data(){
 
 		$("#profilePicture").attr("src", foto_de_perfil);
 		$.each(response.family, function(pos, familiar) {
-	  	console.log("familiar:", familiar.name);
+			console.log("familiar:", familiar.name);
 
 			if (pos==0){
 				add_familiar(familiar.name, familiar.id, " in");
@@ -168,35 +167,35 @@ function get_my_data(){
 
 function get_familiar_data(family_id){
 	var settings = {
-	  "async": true,
-	  "crossDomain": true,
-	  "url": "http://"+url_server+"/families/"+family_id,
-	  "method": "GET",
-	  xhrFields: {
-	    withCredentials: true
-	  },
-	  "headers": {
-	    "cache-control": "no-cache",
-	    "postman-token": "bc75788d-2cae-eda1-4aa1-024dbb0af657"
-	  },
-	  error: function(resp, status){
-	  	console.log(resp);
-		  alert("Error, por favor comprueba tu conexión")
-	  	location.reload();
-	  }
+		"async": true,
+		"crossDomain": true,
+		"url": "http://"+url_server+"/families/"+family_id,
+		"method": "GET",
+		xhrFields: {
+			withCredentials: true
+		},
+		"headers": {
+			"cache-control": "no-cache",
+			"postman-token": "bc75788d-2cae-eda1-4aa1-024dbb0af657"
+		},
+		error: function(resp, status){
+			console.log(resp);
+			alert("Error, por favor comprueba tu conexión")
+			location.reload();
+		}
 	}
 
 	$.ajax(settings).done(function (response) {
-	  // console.log(response);
+		// console.log(response);
 		$.each(response.intolerances, function(pos, intolerancia) {
-	  	// console.log("intolerancia:", intolerancia.name);
-		  add_intolerance(intolerancia.id, family_id);
+			// console.log("intolerancia:", intolerancia.name);
+			add_intolerance(intolerancia.id, family_id);
 		});
 		button_add_intolerance=''+
-		  '<button data-toggle="modal" data-target="#intolerancia-modal" type="button" class="list-group-item">'+
-			  '<a  href=""><span class="add-intolerance glyphicon glyphicon-plus" aria-hidden="true"></span></a>'+
-			  'Agregar Intolerancia'+
-		  '</button>'
+			'<button data-toggle="modal" data-target="#intolerancia-modal" type="button" class="list-group-item">'+
+				'<a  href=""><span class="add-intolerance glyphicon glyphicon-plus" aria-hidden="true"></span></a>'+
+				'Agregar Intolerancia'+
+			'</button>'
 		$(".list-group-"+family_id).append(button_add_intolerance);
 	});
 }
@@ -206,31 +205,31 @@ function new_picture(){
 	form.append("user[avatar]", $("#InputFile").prop("files")[0]);
 
 	var settings = {
-	  "async": true,
-	  "crossDomain": true,
-	  "url": "http://"+url_server+"/user/",
-	  "method": "PUT",
-	  xhrFields: {
-	    withCredentials: true
-	  },
-	  "headers": {
-	    "cache-control": "no-cache",
-	    "postman-token": "1fa9c955-3fc6-3be7-2a0d-d01f59170acb"
-	  },
-	  "processData": false,
-	  "contentType": false,
-	  "mimeType": "multipart/form-data",
-	  "data": form,
-	  error: function(resp, status){
-	  	console.log(resp);
-		  alert("Error, por favor comprueba tu conexión")
-	  	location.reload();
-	  }
+		"async": true,
+		"crossDomain": true,
+		"url": "http://"+url_server+"/user/",
+		"method": "PUT",
+		xhrFields: {
+			withCredentials: true
+		},
+		"headers": {
+			"cache-control": "no-cache",
+			"postman-token": "1fa9c955-3fc6-3be7-2a0d-d01f59170acb"
+		},
+		"processData": false,
+		"contentType": false,
+		"mimeType": "multipart/form-data",
+		"data": form,
+		error: function(resp, status){
+			console.log(resp);
+			alert("Error, por favor comprueba tu conexión")
+			location.reload();
+		}
 	}
 
 	$.ajax(settings).done(function (response) {
-	  console.log(response);
-	  location.reload();
+		console.log(response);
+		location.reload();
 	});
 }
 
@@ -239,36 +238,36 @@ function new_familiar(){
 	form.append("family[name]", $("#new_relative_name").val());
 
 	var settings = {
-	  "async": true,
-	  "crossDomain": true,
-	  "url": "http://"+url_server+"/families",
-	  "method": "POST",
-	  xhrFields: {
-	    withCredentials: true
-	  },
-	  "headers": {
-	    "cache-control": "no-cache",
-	    "postman-token": "04643411-9b85-17f5-0c7f-4c4375d05851"
-	  },
-	  "processData": false,
-	  "contentType": false,
-	  "mimeType": "multipart/form-data",
-	  "data": form,
-	  error: function(resp, status){
-	  	// console.log(resp);
-	  	alert("Error, por favor comprueba tu conexión");
-	  	location.reload();
+		"async": true,
+		"crossDomain": true,
+		"url": "http://"+url_server+"/families",
+		"method": "POST",
+		xhrFields: {
+			withCredentials: true
+		},
+		"headers": {
+			"cache-control": "no-cache",
+			"postman-token": "04643411-9b85-17f5-0c7f-4c4375d05851"
+		},
+		"processData": false,
+		"contentType": false,
+		"mimeType": "multipart/form-data",
+		"data": form,
+		error: function(resp, status){
+			// console.log(resp);
+			alert("Error, por favor comprueba tu conexión");
+			location.reload();
 		}
 	}
 
 	$.ajax(settings).done(function (response) {
 
 		var intolerances = [];
-    $('.btn-add-intolerances').find('input:checked').each(function() {
+		$('.btn-add-intolerances').find('input:checked').each(function() {
 			intolerances.push($(this).data('value'));
-    });
+		});
 
-    new_intolerances(JSON.parse(response).created.id, intolerances);
+		new_intolerances(JSON.parse(response).created.id, intolerances);
 	});
 }
 
@@ -283,94 +282,24 @@ function new_intolerances(familiar, intolerancias){ //(int, array)
 	form.append("intolerances_ids", intolerancias.toString());
 
 	var settings = {
-	  "async": true,
-	  "crossDomain": true,
-	  "url": "http://"+url_server+"/family/intolerance",
-	  "method": "POST",
-	  xhrFields: {
-	    withCredentials: true
-	  },
-	  "headers": {
-	    "cache-control": "no-cache",
-	    "postman-token": "04643411-9b85-17f5-0c7f-4c4375d05851"
-	  },
-	  "processData": false,
-	  "contentType": false,
-	  "mimeType": "multipart/form-data",
-	  "data": form,
-	  error: function(resp, status){
-	  	// alert("Error, no se pudieron agregar las intolerancias, por favor comprueba tu conexión")
-	  	// console.log(resp);
-			if (resp.status==0){
-				alert("Error, por favor comprueba tu conexión")
-			}
-			else{
-				alert(JSON.parse(resp.responseText).error)
-			}
-	  	location.reload();
-		}
-	}
-
-	$.ajax(settings).done(function (response) {
-	  console.log(response);
-	  location.reload();
-	});
-}
-
-// como family_id se usa el stored_id
-function edit_familiar(){
-	var form = new FormData();
-	form.append("family[name]", $("#name").val());
-
-	var settings = {
-	  "async": true,
-	  "crossDomain": true,
-	  "url": "http://"+url_server+"/families/"+stored_id,
-	  "method": "PUT",
-	  xhrFields: {
-	    withCredentials: true
-	  },
-	  "headers": {
-	    "cache-control": "no-cache",
-	    "postman-token": "1082d9ec-0ae4-0939-b9d5-cb671fcecd07"
-	  },
-	  "processData": false,
-	  "contentType": false,
-	  "mimeType": "multipart/form-data",
-	  "data": form,
-	  error: function(resp, status){
-	  	// console.log(resp);
-      if (resp.status==0){
-        alert("Error, por favor comprueba tu conexión")
-		  	// alert("Error, por favor comprueba tu conexión")
-      }
-      else{
-        alert(JSON.parse(resp.responseText).error)
-      }
-	  	location.reload();
-		}
-	}
-
-	$.ajax(settings).done(function (response) {
-	  // console.log(response);
-		location.reload();
-	});
-}
-
-function delete_familiar(){
-	var settings = {
-	  "async": true,
-	  "crossDomain": true,
-	  "url": "http://"+url_server+"/families/"+stored_id,
-	  "method": "DELETE",
-	  xhrFields: {
-	    withCredentials: true
-	  },
-	  "headers": {
-	    "cache-control": "no-cache",
-	    "postman-token": "ffda9024-5df8-49dd-cbbf-af449e968378"
-	  },
-	  error: function(resp, status){
+		"async": true,
+		"crossDomain": true,
+		"url": "http://"+url_server+"/family/intolerance",
+		"method": "POST",
+		xhrFields: {
+			withCredentials: true
+		},
+		"headers": {
+			"cache-control": "no-cache",
+			"postman-token": "04643411-9b85-17f5-0c7f-4c4375d05851"
+		},
+		"processData": false,
+		"contentType": false,
+		"mimeType": "multipart/form-data",
+		"data": form,
+		error: function(resp, status){
+			// alert("Error, no se pudieron agregar las intolerancias, por favor comprueba tu conexión")
+			// console.log(resp);
 			if (resp.status==0){
 				alert("Error, por favor comprueba tu conexión")
 			}
@@ -382,11 +311,81 @@ function delete_familiar(){
 	}
 
 	$.ajax(settings).done(function (response) {
-	  // console.log(response);
+		console.log(response);
+		location.reload();
+	});
+}
+
+// como family_id se usa el stored_id
+function edit_familiar(){
+	var form = new FormData();
+	form.append("family[name]", $("#name").val());
+
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "http://"+url_server+"/families/"+stored_id,
+		"method": "PUT",
+		xhrFields: {
+			withCredentials: true
+		},
+		"headers": {
+			"cache-control": "no-cache",
+			"postman-token": "1082d9ec-0ae4-0939-b9d5-cb671fcecd07"
+		},
+		"processData": false,
+		"contentType": false,
+		"mimeType": "multipart/form-data",
+		"data": form,
+		error: function(resp, status){
+			// console.log(resp);
+			if (resp.status==0){
+				alert("Error, por favor comprueba tu conexión")
+				// alert("Error, por favor comprueba tu conexión")
+			}
+			else{
+				alert(JSON.parse(resp.responseText).error)
+			}
+			location.reload();
+		}
+	}
+
+	$.ajax(settings).done(function (response) {
+		// console.log(response);
+		location.reload();
+	});
+}
+
+function delete_familiar(){
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "http://"+url_server+"/families/"+stored_id,
+		"method": "DELETE",
+		xhrFields: {
+			withCredentials: true
+		},
+		"headers": {
+			"cache-control": "no-cache",
+			"postman-token": "ffda9024-5df8-49dd-cbbf-af449e968378"
+		},
+		error: function(resp, status){
+			if (resp.status==0){
+				alert("Error, por favor comprueba tu conexión")
+			}
+			else{
+				alert(JSON.parse(resp.responseText).error)
+			}
+			location.reload();
+		}
+	}
+
+	$.ajax(settings).done(function (response) {
+		// console.log(response);
 		location.reload();
 	});
 }
 
 function go_main_menu(){
-  window.location = "index.html";
+	window.location = "index.html";
 }
