46e6894feec27f9cfd36dc9cb2ef415eae1a67be
Jorge Caullan <jorge.caullan.12@sansano.usm.cl>
2016-09-27 05:14:01 -0300

    comentar & dar y quitar likes/dislikes a los comentarios

--- www/js/vistas/vista_producto.js
+++ www/js/vistas/vista_producto.js
@@ -19,7 +19,7 @@ function get_my_data(){
   if (!$.isEmptyObject(pdata.matchs)){
     class1 = "alert alert-danger";
     srcimg = "img/nono.png";
-    detalle = '<div class="'+class1+'" style="overflow: auto;" role="alert">\
+    detalle = '<div class="'+class1+'" style="overflow: auto; margin-bottom: 10px;" role="alert">\
                 <h3 style="margin: 0px;">\
                   <div class="row">\
                     <div class="col-xs-3">\
@@ -32,9 +32,13 @@ function get_my_data(){
                 </h3>\
                 <div class="row" style="margin: -10px 0 0 85px; font-size: 16px;" id="cant-eat"> </div>\
               </div>\
-              <span style="color: white;">Posibles Sintomas:</span>\
-              <div class="well" style="overflow: auto;">\
-                <div id="intolerancesMatchs">\
+              <div class="panel panel-danger">\
+                <div class="panel-heading">\
+                  <span>Posibles Sintomas:</span>\
+                </div>\
+                <div class="well panel" style="overflow: auto; margin-bottom: 0;">\
+                  <div id="intolerancesMatchs">\
+                  </div>\
                 </div>\
               </div>\
               <!--<div class="well" style="overflow: auto;">\
@@ -62,11 +66,12 @@ function get_my_data(){
     }
   });
 
+  get_comments();
 
-  $("#product-name").html(pdata.pname)
-  $("#product-id").html(pdata.pid)
-  $("#product-ingredients").html(pdata.ingredients)
-  $("#product-image").attr("src", pdata.image_route)
+  $("#product-name").html(pdata.pname);
+  $("#product-id").html(pdata.pid);
+  $("#product-ingredients").html(pdata.ingredients);
+  $("#product-image").attr("src", pdata.image_route);
 
   // centrar la imagen cuando esta descuadrada (ej: 120x30)
   setTimeout(function(){
@@ -80,10 +85,6 @@ function get_my_data(){
     }
   }, 100);
 }
-
-
-
-
 
 
 function crear_mensaje_problema_con_familiar(nombre_familiar, problemas_intolerancias){
@@ -112,14 +113,199 @@ function crear_mensaje_problema_con_familiar(nombre_familiar, problemas_intolera
 }
 
 
+function get_comments(){
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "http://"+url_server+"/product_comments/"+pdata.pid,
+    "method": "GET",
+    xhrFields: {
+      withCredentials: true
+    },
+    "headers": {
+      "content-type": "application/json",
+      "cache-control": "no-cache",
+      "postman-token": "62f3283b-5118-e38d-7538-2b6ea2f3e39c"
+    },
+    error: function(resp, status){
+      if (resp.status==0){
+        alert("Error al leer los comentarios")
+      }
+      else{
+        send_alert(JSON.parse(resp.responseText).error, "danger");
+      }
+      location.reload();
+    }
+  }
+
+  $.ajax(settings).done(function (response) {
+    // console.log(response);
+    for (i = 0, len = response.comments.length; i < len; i++) {
+      if (response.comments[i]["prom_likes"]>-15) {
+        add_comment(response.comments[i]);
+      }
+      else{
+        $("#product-comments").append('\
+          <div class="panel-body" style="margin-left: 25px; color: red;">\
+            <div class="row">\
+              '+response.comments[i]["user"]["username"]+': <b><i>Comentario bloqueado</i></b>\
+            </div>\
+            <div class="row">\
+              Este comentario fue bloqueado por tener demaciados votos negativos\
+            </div>\
+          </div>');
+      }
+    }
+  });
+}
 
 
+function add_comment(hash_comentario){
+  comment = '\
+  <div class="panel-body">\
+    <div class="col-xs-9">\
+      <div class="row">\
+        '+hash_comentario["user"]["username"]+': <b>'+hash_comentario["title"]+'</b>\
+      </div>\
+      <div class="row">\
+        '+hash_comentario["description"]+'\
+      </div>\
+    </div>\
+    <div class="col-xs-3" style="text-align: center;">\
+      <div id="prom_likes_'+hash_comentario["id"]+'">\
+        '+hash_comentario["prom_likes"]+'\
+      </div>\
+      <a onClick="like('+hash_comentario["id"]+',\'like\')" id="likes_'+hash_comentario["id"]+'">\
+        <span class="glyphicon glyphicon-thumbs-up"></span>\
+      </a>&nbsp;\
+      <a onClick="like('+hash_comentario["id"]+',\'dislike\')" id="dislikes_'+hash_comentario["id"]+'">\
+        <span class="glyphicon glyphicon-thumbs-down"></span>\
+      </a>\
+    </div>\
+  </div>'
+  $("#product-comments").append(comment);
+}
 
+
+function like(id_comentario, like_dislike){
+  var form = new FormData();
+  form.append(like_dislike, "true");
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "http://"+url_server+"/like_comment/"+id_comentario,
+    "method": "PUT",
+    xhrFields: {
+      withCredentials: true
+    },
+    "headers": {
+      "cache-control": "no-cache",
+      "postman-token": "02773620-b093-7aad-3ca1-73e82dfd7283"
+    },
+    "processData": false,
+    "contentType": false,
+    "mimeType": "multipart/form-data",
+    "data": form,
+    error: function(resp, status){
+      if (resp.status==0){
+        alert("Error, por favor comprueba tu conexión");
+      }
+      else{
+        send_alert(JSON.parse(resp.responseText).error, "danger");
+      }
+      location.reload();
+    }
+  }
+
+  $.ajax(settings).done(function (response) {
+    $("#likes_"+id_comentario).removeAttr('onclick');
+    $("#dislikes_"+id_comentario).removeAttr('onclick');
+    prom_likes = parseInt($("#prom_likes_"+id_comentario).text())
+
+    if (like_dislike=="like"){
+      $("#prom_likes_"+id_comentario).text(prom_likes+1)
+      
+      $("#dislikes_"+id_comentario).css('color','gray');
+      $("#likes_"+id_comentario).css('color','black');
+      
+      $("#likes_"+id_comentario).attr('onclick',"like("+id_comentario+",\'like_cancel\')");
+    }
+    else if (like_dislike=="dislike") {
+      $("#prom_likes_"+id_comentario).text(prom_likes-1)
+      
+      $("#likes_"+id_comentario).css('color','gray');
+      $("#dislikes_"+id_comentario).css('color','black');
+      
+      $("#dislikes_"+id_comentario).attr('onclick',"like("+id_comentario+",\'dislike_cancel\')");
+    }
+    else{
+      $("#likes_"+id_comentario).removeAttr('style');
+      $("#dislikes_"+id_comentario).removeAttr('style');
+
+      $("#likes_"+id_comentario).attr('onclick',"like("+id_comentario+",\'like\')");
+      $("#dislikes_"+id_comentario).attr('onclick',"like("+id_comentario+",\'dislike\')");
+
+      if (like_dislike=="like_cancel"){
+        $("#prom_likes_"+id_comentario).text(prom_likes-1)
+      }
+      else if (like_dislike=="dislike_cancel"){
+        $("#prom_likes_"+id_comentario).text(prom_likes+1)
+      }
+    }
+    // console.log("likes: "+JSON.parse(response).likes+", dislikes: "+JSON.parse(response).dislikes);
+  });
+}
+
+
+function comentar(){
+  var form = new FormData();
+  form.append("title", $("#title").val());
+  form.append("description", $("#description").val());
+  form.append("product_id", pdata.pid);
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "http://"+url_server+"/comments",
+    "method": "POST",
+    xhrFields: {
+      withCredentials: true
+    },
+    "headers": {
+      "cache-control": "no-cache",
+      "postman-token": "ec11ee2b-33d1-480d-00a2-bb12c6c834cd"
+    },
+    "processData": false,
+    "contentType": false,
+    "mimeType": "multipart/form-data",
+    "data": form,
+    error: function(resp, status){
+      if (resp.status==0){
+        alert("Error, por favor comprueba tu conexión")
+      }
+      else{
+        send_alert(JSON.parse(resp.responseText).error, "danger");
+      }
+      location.reload();
+    }
+  }
+
+  $.ajax(settings).done(function (response) {
+    $("#comentar-modal").modal('toggle');
+    $("#title").val("");
+    $("#description").val("");
+    // console.log("comentado:"+response);
+    $("#product-comments").html('\
+        <div style="text-align: right;">\
+          <button style="width: 75%; margin: 5px;" type="button" class="btn btn-default" data-toggle="modal" data-target="#comentar-modal">Comentar</button>\
+        </div>');
+    get_comments();
+  });
+}
 
 
 function denunciar(){
-  // var retrievedObject = localStorage.getItem('pdata');
-  // pdata2 = JSON.parse(retrievedObject);
 
   var form = new FormData();
   form.append("product_id", pdata.pid);
@@ -129,6 +315,9 @@ function denunciar(){
     "crossDomain": true,
     "url": "http://"+url_server+"/denounce_product",
     "method": "POST",
+    xhrFields: {
+      withCredentials: true
+    },
     "headers": {
       "cache-control": "no-cache",
       "postman-token": "245521af-53b1-6a74-855b-343d227a24ac"
